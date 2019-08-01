07acf0b3de651fd90007753275f5b6a01959d4e1
Brian Pompey <brianthescholar@gmail.com>
2019-08-01 17:10:51 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,6 +1,6 @@
 def reverse_each_word(phrase)
   words = phrase.split(",")
-  words.collect do |letters|
-    letters.reverse!
-  end
+#  words.collect do |letters|
+#    letters.reverse!
+#  end
 end
